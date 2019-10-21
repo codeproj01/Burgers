430daa7657a788bc67503b8b9e052522dc00f1ac
codeproj01 <cliftonmpls@comcast.net>
2019-10-21 11:48:27 -0500

    3306

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else {
  connection = mysql.createConnection({
-	port: 3307,
+	port: 3306,
 	host: 'localhost',
 	user: 'root',
 	password: 'root',
